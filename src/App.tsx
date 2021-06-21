28d8d4e006a6134968202276f0a42e447f5caf83
Wiliam M. D Epiro <wiliam.depiro@smbb-consultoria.com.br>
2021-06-21 10:47:03 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [count , setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+      <p> Cliques {count}</p>
+      <button className="btn btn-outline-danger" onClick={()=> setCount(count+1)}>Somar</button>
+        
       </header>
     </div>
   );
