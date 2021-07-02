e7672446f721ad4acf855c7df5a987709841572b
Wiliam M. D Epiro <wiliam.depiro@smbb-consultoria.com.br>
2021-07-02 11:56:14 -0300

    subindo teste

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ interface Inputs{
 
 
 function App() {
-  const { register, handleSubmit } = useForm<Inputs>();
+  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
   return (
     <div className="App">
@@ -21,13 +21,20 @@ function App() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="container mb-5">
           <div className="row">
-        <input type="text" className="form-input" placeholder="Login" {...register("login")} />
+        <input type="email" className="form-input" placeholder="Login"  {...register("login",{ required: true, maxLength: 30,
+                        pattern: {
+                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                            message: "Email inválido"
+                        }
+ })} />
+        <p className="text-danger">{errors.login?.message}</p>
         </div>
         <div className="row mt-5">
-        <input type="password" className="form-input" placeholder="Senha" {...register("senha")}/> 
+        <input type="password" className="form-input" placeholder="Senha" required {...register("senha",{ required: true,  minLength:{value:5, message:"Senha deve ter mais que 5 caracteres"} })}/> 
+        <p className="text-danger">{errors.senha?.message}</p>
         </div>
         </div>
-        <button className="btn btn-outline-success">Login</button>
+        <button className="btn btn-primary btn-lg">Login</button>
 
       </form>
       
