aad2479225ca12590967cb5f50eab59322d14b15
Wiliam M. D Epiro <wiliam.depiro@smbb-consultoria.com.br>
2021-07-02 11:01:45 -0300

    login teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
-import React, { useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useForm ,SubmitHandler} from 'react-hook-form';
+
+interface Inputs{
+  login: string;
+  senha: string;
+}
+
 
 function App() {
-  const [count , setCount] = useState(0);
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-      <p> Cliques {count}</p>
-      <button className="btn btn-outline-danger" onClick={()=> setCount(count+1)}>Somar</button>
-        
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="container mb-5">
+          <div className="row">
+        <input type="text" className="form-input" placeholder="Login" {...register("login")} />
+        </div>
+        <div className="row mt-5">
+        <input type="password" className="form-input" placeholder="Senha" {...register("senha")}/> 
+        </div>
+        </div>
+        <button className="btn btn-outline-success">Login</button>
+
+      </form>
+      
+          
       </header>
     </div>
   );
